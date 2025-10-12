757f8aed87e6f35be802e01df13c016e80d78040
Mircea-Pavel Anton <contact@mirceanton.com>
2025-10-13 01:43:13 +0300

    chore: cleanup test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,15 +2,36 @@
 set -e
 
 # Create config directory if it doesn't exist
-export TALOSCONFIG_DIR="./configs/"
-export TALOSCONFIG="./.config"
-export TALSWITCHER_LOG_LEVEL="debug"
+export TALOSCONFIG_DIR="./test/configs/"
+export TALOSCONFIG="./test/config"
 mkdir -p $TALOSCONFIG_DIR
+
+setup() {
+    echo "Performing setup..."
+
+    # Build talswitcher
+    echo "========================================================================================="
+    echo "Building talswitcher..."
+    echo "========================================================================================="
+    go build -o talswitcher
+
+    # Set up talos clusters
+    echo "========================================================================================="
+    echo "Setting up Talos clusters..."
+    echo "========================================================================================="
+    talosctl cluster create --name=test-cluster-1 --talosconfig=./configs/talos1.yaml --cidr=10.5.0.0/24 &
+    talosctl cluster create --name=test-cluster-2 --talosconfig=./configs/talos2.yaml --cidr=10.6.0.0/24 &
+    wait
+
+    echo "========================================================================================="
+    echo "Setup completed."
+    echo "========================================================================================="
+}
 
 cleanup() {
     echo "Performing cleanup..."
 
-    # Stop and delete minikube clusters
+    # Stop and delete talos clusters
     echo "Stopping and deleting test-cluster-1..."
     talosctl cluster destroy --name=test-cluster-1 || true
 
@@ -22,28 +43,6 @@ cleanup() {
     rm -rf ./configs/
 
     echo "Cleanup completed."
-}
-
-setup() {
-    echo "Performing setup..."
-
-    # Build talswitcher
-    echo "========================================================================================="
-    echo "Building talswitcher..."
-    echo "========================================================================================="
-    go build -o talswitcher
-
-    # Set up Kubernetes clusters
-    echo "========================================================================================="
-    echo "Setting up Talos clusters..."
-    echo "========================================================================================="
-    talosctl cluster create --name=test-cluster-1 --talosconfig=./configs/talos1.yaml --cidr=10.5.0.0/24 &
-    talosctl cluster create --name=test-cluster-2 --talosconfig=./configs/talos2.yaml --cidr=10.6.0.0/24 &
-    wait
-
-    echo "========================================================================================="
-    echo "Setup completed."
-    echo "========================================================================================="
 }
 
 run_tests() {
